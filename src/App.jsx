d4347dd95c17721c5ecdf5d86b9469d97f07b540
shubhamshukla3121 <shubhamshukla@winjit.com>
2024-12-30 08:01:42 +0000

    Initial commit

--- src/App.jsx
+++ src/App.jsx
@@ -24,12 +24,12 @@ import "./styles.css";
  * If you don't have one of the social sites listed, leave it as an empty string.
  */
 const siteProps = {
-  name: "Alexandrie Grenier",
-  title: "Web Designer & Content Creator",
-  email: "alex@example.com",
+  name: "Shubham Shukla",
+  title: "Software Developer",
+  email: "shubhamshukla@winjit.com",
   gitHub: "microsoft",
   instagram: "microsoft",
-  linkedIn: "satyanadella",
+  linkedIn: "shubham-shukla-764682141",
   medium: "",
   twitter: "microsoft",
   youTube: "Code",
